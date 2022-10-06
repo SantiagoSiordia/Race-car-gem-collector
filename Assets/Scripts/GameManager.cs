4bf3acaf74346b133e26aa354ce552eb667ed6a5
Santiago Siordia <santiago.gonzalez@wizeline.com>
2022-10-06 16:22:01 -0500

    Reloading Level

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
@@ -42,5 +43,10 @@ public class GameManager : MonoBehaviour
     public void GameOver() {
         isGameStarted = false;
         platformSpawner.SetActive(false);
+        Invoke("ReloadLevel", 1f);
+    }
+
+    void ReloadLevel() {
+        SceneManager.LoadScene("Game");
     }
 }
