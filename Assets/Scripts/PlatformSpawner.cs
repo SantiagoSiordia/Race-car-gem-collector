c65161dd4e9d9cbe7e6191773ad55b5e71a379ba
Santiago Siordia <santiago.gonzalez@wizeline.com>
2022-10-06 15:20:12 -0500

    platform spawner

--- Assets/Scripts/PlatformSpawner.cs
+++ Assets/Scripts/PlatformSpawner.cs
@@ -4,15 +4,42 @@ using UnityEngine;
 
 public class PlatformSpawner : MonoBehaviour
 {
+
+    [SerializeField] private GameObject platformPrefab;
+    public Transform lastPlatform;
+    Vector3 lastPlatformPosition;
+    Vector3 newPosition;
+
+    bool stop = false;
     // Start is called before the first frame update
-    void Start()
-    {
-        
+    void Start() {
+        lastPlatformPosition = lastPlatform.position;
+        StartCoroutine(SpawnPlatforms());
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
+    void Update() {
+    }
+
+    IEnumerator SpawnPlatforms() {
+
+        while (!stop) {
+            GeneratePosition();
+            GameObject newPlatform = Instantiate(platformPrefab, newPosition, Quaternion.identity);
+            lastPlatform = newPlatform.transform;
+            lastPlatformPosition = lastPlatform.position;
+            yield return new WaitForSeconds(0.4f);
+        }
+    }
+
+    void GeneratePosition() {
+        newPosition = lastPlatformPosition;
+        int random = Random.Range(0, 2);
+        if(random == 0) {
+            newPosition.x += 2f;
+        }
+        else {
+            newPosition.z += 2f;
+        }
     }
 }
